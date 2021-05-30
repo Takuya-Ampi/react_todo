62ce4eb72405deb56d86ae266c64325de180d737
Takuya-Ampi <53106671+Takuya-Ampi@users.noreply.github.com>
2021-05-30 20:54:53 +0900

    css途中まで

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import "./styles.css";
 
 export const App = () => {
   return (
     <>
-      <h1>Hello</h1>
-      <p>change commit</p>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>aaa</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>iii</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <div>
+            <li>uuu</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
     </>
   );
 };
